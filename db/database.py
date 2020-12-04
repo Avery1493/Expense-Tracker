4b6742f6b3460814566f25f09073aa53c905dac4
Avery Quinn <avery.quinn1493@gmail.com>
2020-12-04 06:17:41 -0800

    expense table created

--- db/database.py
+++ db/database.py
@@ -9,10 +9,36 @@ def create_connection(db_file):
         print(sqlite3.version)
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def main():
+    database = r"C:\Users\avery\Lambda\Expense-Tracker\db\exp.db"
+
+    sql_create_expense_table = """ CREATE TABLE IF NOT EXISTS expenses (
+        id integer PRIMARY KEY,
+        title text NOT NULL,
+        amount float,
+        created_at text,
+        tags text
+    ); """
+
+    conn = create_connection(database)
+
+    if conn is not None:
+        create_table(conn, sql_create_expense_table)
+    else:
+        print("Error! Cannot create the database connection")
 
 
 if __name__ == "__main__":
-    create_connection(r"C:\Users\avery\Lambda\Expense-Tracker\db\exp.db")
+    main()
